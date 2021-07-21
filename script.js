3c3e9a3dbbef65d3c256736e58e7df15ceb3b8e7
wangynj <jeslinwang@gmail.com>
2021-07-21 18:19:08 +0800

    level 2 SPS

--- script.js
+++ script.js
@@ -1,66 +1,77 @@
-    
-var PlayervsComputer = function (PlayerChoice, ComputerChoice){
-
-  if (PlayerChoice =='scissors' && ComputerChoice =='scissors'){
-  return myOutputValue= 'Its a tie!';
-  }
-
-  if (PlayerChoice =='scissors' && ComputerChoice =='paper'){
-  return myOutputValue= 'you win';
-  }
-
-  if (PlayerChoice =='scissors' && ComputerChoice =='stone'){ 
-  return myOutputValue= 'you lose';
-  }
-  if (PlayerChoice =='paper' && ComputerChoice =='paper') {
-  return myOutputValue= 'Its a tie!';
-  }
-
-  if (PlayerChoice =='paper' && ComputerChoice =='stone')
-  return myOutputValue= 'you win';
-
-  if (PlayerChoice =='paper' && ComputerChoice =='scissors')
-  return myOutputValue= 'you lose';
-
-  if (PlayerChoice =='stone' && ComputerChoice =='stone')
-  return myOutputValue= 'Its a tie!';
-
-  if (PlayerChoice =='stone' && ComputerChoice =='paper')
-  return myOutputValue= 'you lose';
-
-  if (PlayerChoice =='stone' && ComputerChoice =='scissors')
-  return myOutputValue= 'you win';
-
-  return myOutputValue;
-
-};
-var generateaRandomInteger= function () {
-var randomDecimal = Math.random() * 3;
-var randomIntegar = Math.floor(randomDecimal) + 1;
-return randomIntegar;
-};
-
-var generateaRandomSPR= function(){
-var randomizednum = generateaRandomInteger();
-if (randomizednum == 1) {
-  return 'scissors';
-  }
-if (randomizednum == 2) {
-  return 'stone';
-  }
-  if (randomizednum == 3) {
-    return 'paper';
-  };
-
-};
-
+//user inputs their name
+var startGame = `please enter your name`;
+var winScore = 0;
+var playerTries = 0;
+// user inputs scissors, paper or stone, output will determine win lose or draw or wrong spelling
 var main = function (input) {
-var PlayerChoice = input
-console.log(PlayerChoice)
-var ComputerChoice = generateaRandomSPR()
-console.log(ComputerChoice)
-var WhoWins = PlayervsComputer(PlayerChoice,ComputerChoice)
-return WhoWins
-var myOutputValue = generaterandomSCR (input);
+  var myOutputValue = ``;
+  playerTries = playerTries + 1;
+  //do this if game mode is waiting for enter your name
+  if (startGame == `please enter your name`) {
+    //set the name
+    userName = input;
+    //now that we have the name, switch mode
+    startGame = `SPS game`;
+    playerTries = playerTries - 1;
+    myOutputValue = `Welcome  ` + userName;
+  }
+  //if gamemode is sps game
+  else if (startGame == `SPS game`) {
+  }
+  var computerChoice = SPS();
+  var myOutputValue =
+    `Hello ` +
+    userName +
+    `    please enter any of the following: scissors, paper, stone to proceed!`;
+  //scissors =1 SPS
+  //paper = 2
+  //stone =3
+  if (input == `scissors` && computerChoice == 1) {
+    myOutputValue =
+      userName + `   ` + `<br> its a draw! the computer chose scissors too!`;
+  }
+  if (input == `scissors` && computerChoice == 2) {
+    winScore = winScore + 1;
+    myOutputValue =
+      userName + `   ` + `<br> you win! the computer chose paper!`;
+  }
+  if (input == `scissors` && computerChoice == 3) {
+    myOutputValue =
+      userName + `   ` + `<br> you lose! the computer chose stone!`;
+  }
+  if (input == `paper` && computerChoice == 1) {
+    myOutputValue =
+      userName + `   ` + `<br> you lose! computer chose scissors!`;
+  }
+  if (input == `paper` && computerChoice == 2) {
+    myOutputValue =
+      userName + `   ` + `<br> its a draw! the computer chose paper too!`;
+  }
+  if (input == `paper` && computerChoice == 3) {
+    winScore = winScore + 1;
+    myOutputValue = userName + `   ` + `<br> you win! computer chose stone!`;
+  }
+  if (input == `stone` && computerChoice == 1) {
+    winScore = winScore + 1;
+    myOutputValue =
+      userName + `   ` + `<br> you win! the computer chose scissors!`;
+  }
+  if (input == `stone` && computerChoice == 2) {
+    myOutputValue =
+      userName + `   ` + `<br> you lose! the computer chose paper!`;
+  }
+  if (input == `stone` && computerChoice == 3) {
+    myOutputValue =
+      userName + `   ` + `<br> its a draw! the computer chose stone too!`;
+  }
+  myOutputValue =
+    myOutputValue + Math.ceil((winScore / playerTries) * 100) + `%`;
   return myOutputValue;
-}; 
+};
+//Random outcome
+SPS = function () {
+  var randomNumberGenerator = Math.random() * 3;
+  var randomIntegar = Math.ceil(randomNumberGenerator);
+  console.log(randomIntegar);
+  return randomIntegar;
+};
